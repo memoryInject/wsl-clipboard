0bab5ad0a04ca7d6673c1146197543f2fa736a38
Mahesh MS <msmahesh007@gmail.com>
2022-09-30 19:32:24 +0800

    Fix argument count output and added additional comments to code.
      - Removed argument count show in console output
      - Added detailed comments to code.

--- wclip.cpp
+++ wclip.cpp
@@ -69,22 +69,23 @@ int wmain(int argc, wchar_t* argv[])
     if (IsValidCodePage(CP_UTF8)) {
 	    SetConsoleOutputCP(CP_UTF8);
     }
-	//SetConsoleOutputCP(CP_UTF8);
-    std::cout << argc << std::endl;
 
-    // If we don't pass any argument when executing eg., `./wclip.exe` or
-    // we pass argument like './wclip.exe -o' or 'wclip.exe --output' 
-    // output contents from clipboard
+    // If we don't pass any argument when executing `./wclip.exe`
+    // output the contents from clipboard
     if (argc == 1) {
         get_clipboard_text();
         return 0;
     }
 
+    // If we pass argument like './wclip.exe -o' or 'wclip.exe --output' 
+    // output the contents from clipboard
     if (argc == 2 && (output_args.find(argv[1]) != output_args.end())) {
         get_clipboard_text();
         return 0;
     }
 
+    // If we pass argument like './wclip.exe -i "Hello World!"' or 'wclip.exe --input "Hello World!"' 
+    // set the clipboard content to passed 3rd argument: "Hello World!"
     if (argc == 3 && (input_args.find(argv[1]) != input_args.end())) {
         set_clipboard_text(argv[2]);
         return 0;
@@ -111,6 +112,7 @@ int wmain(int argc, wchar_t* argv[])
         return 0;
     }
 
+    // Help argument: `wclip.exe -h` or `wclip.exe --help`
     if (argc == 2 && (help_args.find(argv[1]) != help_args.end())) {
         std::cout 
             << "usage: wclip.exe [<flags>]\n\n" 
